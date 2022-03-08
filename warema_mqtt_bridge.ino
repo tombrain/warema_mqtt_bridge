7085fbaef67d5021efd581f2fa467aaaba295d9f
Stefan Meyer <git@meyersonline.de>
2022-03-08 16:22:05 +0100

    Mit LEDs

--- warema_mqtt_bridge.ino
+++ warema_mqtt_bridge.ino
@@ -1,26 +1,49 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #include "RCSwitchWarema.h"
 #include "secure.h"
 
+#define GPIO_D1 5
+#define GPIO_D5 14
+#define GPIO_D6 12
+#define GPIO_D7 13
+
+#define LED_WIFI_RED GPIO_D1
+#define LED_WIFI_YELLOW GPIO_D5
 
 // MQTT Broker
 const char *mqtt_broker{"homeassistant.fritz.box"};
 const char *topic{"cmnd/rf-warema-bridge/data"};
+const char *topic_state_connection{"stat/rf-warema-bridge/connection"};
 const auto mqtt_port{1883};
 
 
-WiFiClient espClient{};
-PubSubClient client{espClient};
+WiFiClient wifiClient{};
+PubSubClient mqttClient{wifiClient};
 RCSwitchWarema mySwitch{};
+
+
+void writeConnected(bool const& state)
+{
+   // rote LED
+   digitalWrite(LED_WIFI_RED, state ? LOW : HIGH);
+   // gelbe LED
+   digitalWrite(LED_WIFI_YELLOW, state ? HIGH : LOW );
+}
 
 void setup() {
   // Set software serial baud to 115200;
   Serial.begin(115200);
 
+  pinMode(LED_WIFI_RED, OUTPUT);
+  pinMode(LED_WIFI_YELLOW, OUTPUT);
+
+  writeConnected(false);
+
   mySwitch.enableTransmit(10);  // Der Sender wird an Pin 10 angeschlossen
-  
+
   // connecting to a WiFi network
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -29,23 +52,24 @@ void setup() {
   }
   Serial.println("Connected to the WiFi network");
   //connecting to a mqtt broker
-  client.setServer(mqtt_broker, mqtt_port);
-  client.setCallback(callback);
-  while (!client.connected()) {
-      String client_id = "rf-warema-bridge-client-";
+  mqttClient.setServer(mqtt_broker, mqtt_port);
+  mqttClient.setCallback(callback);
+  while (!mqttClient.connected()) {
+      String client_id = "rf-warema-bridge-mqttClient-";
       client_id += String(WiFi.macAddress());
-      Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
-      if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+      Serial.printf("The mqttClient %s connects to the public mqtt broker\n", client_id.c_str());
+      if (mqttClient.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
           Serial.println("Public warema-bridge mqtt broker connected");
       } else {
           Serial.print("failed with state ");
-          Serial.print(client.state());
+          Serial.print(mqttClient.state());
           delay(2000);
       }
   }
   // publish and subscribe
-  client.publish(topic, "hello emqx");
-  client.subscribe(topic);
+  mqttClient.publish(topic, "hello emqx");
+  mqttClient.subscribe(topic);
+  sendMQTTClientInfos();
 }
 
 void callback(char *topic, byte *payload, unsigned int length)
@@ -54,8 +78,8 @@ void callback(char *topic, byte *payload, unsigned int length)
   Serial.println(topic);
   Serial.print("Message:");
 
-  char *command = new char[length + 1];  
-  
+  char *command = new char[length + 1];
+
   for (int i = 0; i < length; i++)
   {
     command[i] = payload[i];
@@ -69,6 +93,29 @@ void callback(char *topic, byte *payload, unsigned int length)
   Serial.println("-----------------------");
 }
 
-void loop() {
-  client.loop();
+void loop()
+{
+  writeConnected(mqttClient.loop());
+}
+
+void sendMQTTClientInfos()
+{
+  StaticJsonDocument<250> doc;
+
+  // {"ClientName": <string>, "IP": <string>, "MAC": <string>, "RSSI": <string>, "HostName": <string>, "ConnectedSSID": <string>}
+  doc["IP"] = WiFi.localIP();
+  doc["MAC"] = WiFi.macAddress();
+  doc["RSSI"] = WiFi.RSSI();
+  doc["HostName"] = WiFi.hostname();
+  doc["ConnectedSSID"] = WiFi.SSID();
+
+  std::string jsonOut{};
+
+  serializeJson(doc, jsonOut);
+
+  auto const packetIdPub1{mqttClient.publish(topic_state_connection, jsonOut.c_str())};
+  Serial.printf("Publishing on topic %s at QoS 1, packetId: %i ", topic_state_connection, packetIdPub1);
+
+  serializeJson(doc, Serial);
+  Serial.printf("MessageJson: %s \n", jsonOut.c_str());
 }
